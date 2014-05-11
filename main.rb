0c98534a32ad8e7687c0adada73f950c27bfe05b
Regine Walls <regineparedes@ymail.com>
2014-05-11 13:15:34 +0800

    aling_nena Paredes 123072

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require './boot.rb'
+require './money_calculator.rb'
 
 # ROUTES FOR ADMIN SECTION
 get '/admin' do
@@ -43,3 +44,63 @@ get '/delete_product/:id' do
   redirect to '/admin'
 end
 # ROUTES FOR ADMIN SECTION
+
+get '/' do
+  random = Item.all
+  @products = random.sample(10)
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/products' do
+  @products = Item.all
+  erb :products
+end
+
+get '/purchase/:id' do
+  @product = Item.find(params[:id])
+  erb :purchase_form
+end
+
+post '/product_purchase/:id' do
+  alerts = ""
+  @alerts = alerts
+
+  @product = Item.find(params[:id])
+
+  quantity = params[:quantity]
+  @quantity = quantity
+  @total_cost = @quantity.to_i * @product.price
+ 
+  if quantity.to_i > @product.quantity.to_i
+    alerts = "There are insufficient stock for your transaction."
+    @alerts = alerts
+
+  else
+    @alerts = alerts
+    mc = MoneyCalculator.new params[:ones].to_i, params[:fives].to_i, params[:tens].to_i, params[:twenties].to_i, params[:fifties].to_i, params[:hundreds].to_i, params[:five_hundreds].to_i, params[:thousands].to_i
+    @money_given = mc.total
+    @hash = mc.change(@total_cost)
+
+    if @total_cost.to_i > @money_given.to_i
+  	alerts = "You have entered insufficient money for your transaction."
+  	@alerts = alerts
+  
+    else
+  	@change = @money_given - @total_cost
+	left = @product.quantity.to_i - @quantity.to_i
+	@product.sold = @product.sold + @quantity.to_i
+	@product.update_attributes!(
+        quantity: left,
+        )
+  	erb :confirmation
+  end
+end
+end
+
+post '/confirmation/:id' do
+  @product = Item.find(params[:id])
+end
--- money_calculator.rb
+++ money_calculator.rb
@@ -1,12 +1,48 @@
 class MoneyCalculator
 
   def initialize(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands)
-    # each parameter represents the quantity per denomination of money
-    # these parameters can be assigned to instance variables and used for computation
+  	@total_ones = 1 * ones.to_i
+	@total_fives = 5 * fives.to_i
+	@total_tens = 10 * tens.to_i
+	@total_twenties = 20 * twenties.to_i
+	@total_fifties = 50 * fifties.to_i
+	@total_hundreds = 100 * hundreds.to_i
+	@total_five_hundreds = 500 * five_hundreds.to_i
+	@total_thousands = 1000 * thousands.to_i
 
-    # add a method called 'change' that takes in a parameter of how much an item costs
-    # and returns a hash of how much change is to be given
-    # the hash will use the denominations as keys and the amount per denomination as values
-    # i.e. {:fives => 1, fifties => 1, :hundreds => 3}
+	@total = @total_ones.to_i + @total_fives.to_i + @total_tens.to_i + @total_twenties.to_i + @total_fifties.to_i + @total_hundreds.to_i + @total_five_hundreds.to_i + @total_thousands.to_i
+
+	@bills = Hash.new
+	@bills["thousands_bill"] = 0	
+	@bills["five_hundreds_bill"] = 0
+	@bills["hundreds_bill"] = 0
+	@bills["fifties_bill"] = 0
+	@bills["twenties_bill"] = 0
+	@bills["tens_bill"] = 0
+	@bills["fives_bill"] = 0
+	@bills["ones_bill"] = 0
+	
   end
+
+  def total
+	@total = @total_ones.to_i + @total_fives.to_i + @total_tens.to_i + @total_twenties.to_i + @total_fifties.to_i + @total_hundreds.to_i + @total_five_hundreds.to_i + @total_thousands.to_i
+    return @total
+  end
+
+  def change(total_cost)
+	@change = @total - total_cost.to_i
+
+	change = @change
+	grand = change/1000
+	halfgrand = (change%1000)/500
+	tenth = ((change%1000)%500)/100
+	halftenth = (((change%1000)%500)%100)/50
+	twentieth = ((((change%1000)%500)%100)%50)/20
+	halftwentieth = (((((change%1000)%500)%100)%50)%20)/10
+	fifth = ((((((change%1000)%500)%100)%50)%20)%10)/5
+	singles = ((((((change%1000)%500)%100)%50)%20)%10)%5
+	return {:ones_bill => singles, :fives_bill => fifth, :tens_bill => halftwentieth, :twenties_bill => twentieth, :fifties_bill => halftenth, :hundreds_bill => tenth, :five_hundreds_bill => halfgrand, :thousands_bill => grand}
+  end
+
+
 end
